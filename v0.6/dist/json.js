c97f8548152b82eefe7df2262cd7466260b51e57
Joachim Kuebart <joachim.kuebart@gmail.com>
2017-01-20 19:55:35 +0100

    Add version 0.6 to supporting website.

---
+++ v0.6/dist/json.js
@@ -0,0 +1,2 @@
+define(function(n,t){"use strict";t.load=function(t,o,e){n(["./text"],function(n){n.load(t,o,e,{type:"json"})})}});
+//# sourceMappingURL=json.js.map
---
+++ v0.6/dist/xml.js
@@ -0,0 +1,2 @@
+define(function(t,n){"use strict";n.load=function(n,e,o){t(["./text"],function(t){t.load(n,e,o,{type:"document"})})}});
+//# sourceMappingURL=xml.js.map

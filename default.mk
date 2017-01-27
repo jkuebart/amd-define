bd6319e3b7d96a656783df18b90635a438135dc7
Joachim Kuebart <joachim.kuebart@gmail.com>
2017-01-27 22:40:46 +0100

    DRY Makefiles.

--- default.mk
+++ default.mk
@@ -27,4 +27,7 @@
 # ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
 # POSSIBILITY OF SUCH DAMAGE.
 
-.include "../../default.mk"
+PROG_JS		?=	${.CURDIR:T}.js
+JSDIR		?=	/dist
+
+.include "mk/js.prog.mk"
